836defe03e846977486a0f05044fcea88e25ec60
Sam Souza <innovatecodes@gmail.com>
2023-06-25 09:28:27 -0300

    Atributos aria foram adicionados

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const content = document.querySelector('.content');
 bar.addEventListener('click', iconToggle);
 function iconToggle() {
   content.classList.toggle('active');
+  if (content.classList.contains('active')) {
+    bar.setAttribute('aria-pressed', true);
+    bar.setAttribute('aria-label', 'Fechar menu');
+    bar.setAttribute('aria-expanded', true);
+  } else {
+    bar.setAttribute('aria-pressed', false);
+    bar.setAttribute('aria-label', 'Abrir menu');
+    bar.setAttribute('aria-expanded', false)
+  }
 }
